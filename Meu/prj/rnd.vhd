7dc743ca7c47f424f2603780bc493a0d3fd9b6bf
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-03 18:54:35 -0300

    Update rnd.vhd

--- Meu/prj/rnd.vhd
+++ Meu/prj/rnd.vhd
@@ -1,32 +1,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.math_real.all;
-use ieee.numeric_std.all;
 
-entity rnd_2b_gen is
+entity rnd is
 port(
-	clk: in std_logic;
-    bits: out std_logic_vector(1 downto 0)
+	clk, reset: in std_logic;
+    genned: out  std_logic_vector(1 downto 0)
 );
-end rnd_2b_gen;
+end rnd;
 
-architecture gen of rnd_2b_gen is
+architecture gen of rnd is
+
+	signal count_i: std_logic_vector(3 downto 0);
+    signal fb: std_logic;
+
 begin
-	process(clk) is
-    	variable seed1, seed2: positive := 1;
-        variable genned: real;
+	fb <= (not count_i(3)) xor count_i(2);
+    
+    process(clk, reset) is
     begin
-    	if rising_edge(clk) then
-        	uniform(seed1, seed2, genned);
-            if genned < 0.25 then
-            	bits <= "00";
-            elsif genned > 0.25 and genned < 0.5 then
-            	bits <= "01";
-            elsif genned > 0.5 and genned < 0.75 then
-            	bits <= "10";
-            else
-            	bits <= "11";
-            end if;
+    	if reset = '1' then
+        	count_i <= (others=>'0');
+        elsif rising_edge(clk) then
+        	count_i <= count_i(2 downto 0) & fb;
         end if;
     end process;
+    
+    genned(1) <= count_i(2);
+    genned(0) <= count_i(1);
+    
 end gen;
