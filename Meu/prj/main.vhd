16d14050b9d1d5fd3250ccaf9f97f82271ff9f1d
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-04 11:51:41 -0300

    Update main.vhd

--- Meu/prj/main.vhd
+++ Meu/prj/main.vhd
@@ -1,116 +1,135 @@
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all ;
-use ieee.std_logic_unsigned.all;
 
 entity projeto is
 port(
-
-	clk: in std_Logic;
-	
-	a_in: in std_logic;
-	b_in: in std_logic;
+	clk, reset: in std_logic := '0';
+	a_in, b_in: in std_logic := '0';
+    --sgn: out std_logic_vector(1 downto 0) := "00";
     
-    pnt1_disp: out std_logic_vector(7 downto 0) := "11111111";
-    pnt2_disp: out std_logic_vector(7 downto 0) := "11111111";
+    --pnt_a: out std_logic_vector(3 downto 0) := "0000";
+    --pnt_b: out std_logic_vector(3 downto 0) := "0000";
     
-	 clk_led: out std_logic;
+	 signs_word: out std_logic_vector(3 downto 0) := "0000";
+	 clk_led: out std_logic := '0';
 	 
-    signals_word: out std_logic_vector(3 downto 0)
-    
-	);
+	 pntA_word: out std_logic_vector(3 downto 0);
+	 
+    pnt_a_disp: out std_logic_vector(7 downto 0) := "00000000";
+    pnt_b_disp: out std_logic_vector(7 downto 0) := "00000000"
+    );
 end projeto;
 
 architecture bhv of projeto is
-
-	component rnd_2b_gen is
+	
+    component display is
+  	port(
+    	bin_in: in std_logic_vector(3 downto 0) := "0000";
+      	disp_out: out std_logic_vector(7 downto 0) := "00000000"
+  	);
+  	end component;
+    
+    component rnd is
     port(
-    	clk: in std_logic;
-    	bits: out std_logic_vector(1 downto 0)
+        clk, reset: in std_logic := '0';
+        genned: out  std_logic_vector(1 downto 0) := "00"
     );
     end component;
     
-    component display is
-    	port(
-        	bin_in: in std_logic_vector(3 downto 0);
-            disp_out: out std_logic_vector(7 downto 0)
-         );
-    end component; 
-
-	signal pnt1, pnt2: std_logic_vector(3 downto 0) := "0000";
-	signal clk_rnd: std_logic;
-    signal sign_rnd: std_logic_vector(1 downto 0);
-
+	 signal clk_rnd: std_logic := '0';
+    signal sgn_rnd: std_logic_vector(1 downto 0) := "00";
+    
+    signal pnt_a: std_logic_vector(3 downto 0) := "0000";
+    signal pnt_b: std_logic_vector(3 downto 0) := "0000";
+    
+    signal prv_a: std_logic_vector(3 downto 0) := "0000";
+    signal prv_b: std_logic_vector(3 downto 0) := "0000";
+	 
+    
+begin
+	--RND_DUT: rnd port map(clk, reset, sgn_rnd);
+	
+	process(clk) is
+		variable clk_count: integer := 0;
 	begin
-			--RANDDUT: rnd_2b_gen port map(clk_rnd, sign_rnd);
-        
-      process(clk) is
-			variable count_clk_rnd: integer := 0;
-		begin
-			if clk'event and clk = '1' then
-				count_clk_rnd := count_clk_rnd + 1;
-				report "count: " & integer'image(count_clk_rnd) severity note;
-
-			  if count_clk_rnd < 50000000 then
-					clk_rnd <= '1';
-			  elsif count_clk_rnd >= 50000000 AND count_clk_rnd < 100000000 then
-					clk_rnd <= '0';
-			  else
-					count_clk_rnd := 0;
-			  end if;
+		if rising_edge(clk) then
+			clk_count := clk_count + 1;
+			if clk_count < 50000000 then
+				clk_rnd <= '1';
+			elsif clk_count >= 50000000 AND clk_count < 100000000  then
+				clk_rnd <= '0';
+			else
+				clk_count := 0;
 			end if;
-      end process;
-		
-		process(clk_rnd)
+		end if;
+	end process;
+	
+	process(clk_rnd)
 		begin
 			if clk_rnd'event and clk_rnd = '1' then
-				if sign_rnd = "00" then
-					sign_rnd <= "01";
-				elsif sign_rnd = "01" then
-					sign_rnd <= "10";
-				elsif sign_rnd = "10" then
-					sign_rnd <= "11";
-				elsif sign_rnd = "11" then
-					sign_rnd <= "00";
+				if sgn_rnd = "00" then
+					sgn_rnd <= "01";
+				elsif sgn_rnd = "01" then
+					sgn_rnd <= "10";
+				elsif sgn_rnd = "10" then
+					sgn_rnd <= "11";
+				elsif sgn_rnd = "11" then
+					sgn_rnd <= "00";
 				end if;
 			end if;
 		end process;
- 
-		
-		process(sign_rnd, a_in, b_in)
-      begin
-		  
-        	if sign_rnd = "11" then
-            	if a_in = '1' then
-                	pnt1 <= pnt1 + 1;
-                end if;
-                if b_in = '1' then
-                	pnt2 <= pnt2 + 1;
-                end if;
-            else
-            	if a_in = '1' and pnt1 > "0000" then
-                	pnt1 <= pnt1 - 1;
-                end if;
-                if b_in = '1' and pnt2 > "0000" then
-                	pnt2 <= pnt2 - 1;
-                end if;
-            end if;
+			
+	
+
+	process(sgn_rnd, a_in, b_in) is
+    begin
+    	if sgn_rnd = "11" then
+			if a_in = '0'	then
+				pnt_a <= std_logic_vector(unsigned(prv_a) + 1);
+				prv_a <= pnt_a;
+			end if;
             
-            if pnt1 = "1001" or pnt2 = "1001" then
-            	pnt1 <= "0000";
-                pnt2 <= "0000";
-            end if;
-        end process;
-        
-		  clk_led <= clk_rnd;
-		  
-		  with sign_rnd select
-				signals_word <= "0001" when "00",
-										"0010" when "01",
-										"0100" when "10",
-										"1000" when "11";
-										
-			DISP1DUT: display port map(pnt1, pnt1_disp);
-			DISP2DUT: display port map(pnt2, pnt2_disp);
-		  
+			if b_in = '0' then
+          	pnt_b <= std_logic_vector(unsigned(prv_b) + 1);
+            prv_b <= pnt_b;
+			end if;
+      else
+        	if a_in = '0' and pnt_a /= "0000" then
+            pnt_a <= std_logic_vector(unsigned(prv_a) - 1);
+            prv_a <= pnt_a;
+         end if;
+            
+         if b_in = '0' and pnt_b /= "0000" then
+           	pnt_b <= std_logic_vector(unsigned(prv_b) - 1);
+            prv_b <= pnt_b;
+         end if;
+      end if;
+    end process;
+    
+    DISPA_DUT: display port map(pnt_a, pnt_a_disp);
+    DISPB_DUT: display port map(pnt_b, pnt_b_disp);
+	 
+	 clk_led <= clk_rnd;
+	 pntA_word <= pnt_a;
+	 
+	 with sgn_rnd select
+		signs_word <= "0001" when "00",
+						  "0010" when "01",
+						  "0100" when "10",
+						  "1000" when "11";
+--     with pnt_a select
+-- 		pnt_a_disp <= "11000000" when "0000", --0
+-- 			        "11111001" when "0001", --1
+-- 					"10100100" when "0010", --2
+-- 					"10110000" when "0011", --3
+-- 				    "10011001" when "0100", --4
+-- 				    "10010010" when "0101", --5
+-- 				    "10000010" when "0110", --6
+-- 				    "11111000" when "0111", --7
+-- 				    "10000000" when "1000", --8
+-- 				    "10010000" when "1001", --9 
+-- 				    "00000000" when others;
+    
 end bhv;
