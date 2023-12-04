34e0baba99f78540011068d0cdd7e45d57da014c
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-03 23:49:50 -0300

    Update tb.vhd

--- Meu/prj/tb.vhd
+++ Meu/prj/tb.vhd
@@ -1,87 +1,146 @@
+-- Code your testbench here
 library IEEE;
 use IEEE.std_logic_1164.all;
-use ieee.numeric_std.all ;
-use ieee.std_logic_unsigned.all;
 
 entity tb is
 end tb;
 
-architecture bhv of tb is
+architecture a of tb is
 
-	component main is
-	port(
-        a_in: in std_logic;
-	b_in: in std_logic;
-    
-    pnt1_disp: out std_logic_vector(7 downto 0);
-    pnt2_disp: out std_logic_vector(7 downto 0);
-    signals_word: out std_logic_vector(3 downto 0)
-      	--clk_rnd_in: in std_logic_vector(1 downto 0)
+component main is
+port(
+	clk, reset: in std_logic;
+    a_in,b_in: in std_logic; 
+    --sgn: out std_logic_vector(1 downto 0) := "00";
+    --w: out std_logic;
+--     pnt_a: out std_logic_vector(3 downto 0) := "0000";
+--     pnt_b: out std_logic_vector(3 downto 0) := "0000";
+    pnt_a_disp: out std_logic_vector(7 downto 0) ;
+    pnt_b_disp: out std_logic_vector(7 downto 0) 
     );
-	end component;
-    
-    component rnd_2b_gen is
+end component;
+
+component rnd is
     port(
-    	seed1, seed2: in positive;
-    	bits: out std_logic_vector(1 downto 0)
+        clk, reset: in std_logic;
+        genned: out  std_logic_vector(1 downto 0)
     );
     end component;
-    
-    component display is
-    	port(
-        	bin_in: in std_logic_vector(3 downto 0);
-            disp_out: out std_logic_vector(7 downto 0)
-         );
-    end component; 
 
-	signal a_in, b_in: std_logic;
-    signal pnt1_disp, pnt2_disp: std_logic_vector(7 downto 0);
-    signal signals_word: std_logic_vector(3 downto 0);
-    
-	signal clk_rnd_in: std_logic_vector(1 downto 0);
-    signal pnt1, pnt2: std_logic_vector(3 downto 0) := "0000";
+component display is
+  	port(
+    	bin_in: in std_logic_vector(3 downto 0) := "0000";
+      	disp_out: out std_logic_vector(7 downto 0)
+  	);
+  	end component;
 
-	begin
-    	MAINDUT: main port map(a_in, b_in);
-        RANDDUT: rnd_2b_gen port map(1, 1, clk_rnd_in);
-        DISP1DUT: display port map(pnt1, pnt1_disp);
-        DISP2DUT: display port map(pnt2, pnt2_disp);
+signal a,b, clk, res: std_logic;
+--signal sgn: std_logic_vector(1 downto 0);
+signal p1, p2: std_logic_vector(3 downto 0);
+signal disp1, disp2: std_logic_vector(7 downto 0) ;
+
+begin
+	m: main port map(clk, res, a,b, disp1, disp2);
+    --DISPA_DUT: display port map(p1, disp1);
+--     DISPB_DUT: display port map(p2, disp2);
+    
+    process
+    begin
+    	clk <= '0';
+        res <= '0';
+        a <= '0';
+        b <= '0';
+        wait for 5 ns;
         
-		process
-        begin
-        	a_in <= '0';
-          	b_in <= '0';
-          	wait for 1 ns;
-          	
-          	a_in <= '1';
-          	b_in <= '0';
-          	wait for 1 ns;
-          
-          	a_in <= '0';
-          	b_in <= '1';
-          	wait for 1 ns;
-          
-          	a_in <= '1';
-          	b_in <= '1';
-          	wait for 1 ns;
-          
-          	a_in <= '1';
-          	b_in <= '0';
-          	wait for 1 ns;
-          
-          	a_in <= '1';
-          	b_in <= '1';
-          	wait for 1 ns;
-          
-          	a_in <= '0';
-          	b_in <= '1';
-          	wait for 1 ns;
-          
-          	a_in <= '0';
-          	b_in <= '0';
-          	wait for 1 ns;
-          
-          	wait;
-        end process;
+        clk <= not clk; -- 01
+        a <= '0';
+        wait for 5 ns;
         
-end bhv;
+        clk <= not clk; -- 01
+        wait for 5 ns;
+        
+        clk <= not clk; -- 10
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --10
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '1';
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 00
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 00
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 01
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 01
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --10
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --10
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '1';
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '1';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 00
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 00
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 01
+        a <= '1';
+        wait for 5 ns;
+        
+        clk <= not clk; -- 01
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --10
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --10
+        a <= '0';
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '1';
+        wait for 5 ns;
+        
+        clk <= not clk; --11
+        a <= '0';
+        wait for 5 ns;
+        
+        
+        wait;
+        
+    end process;
+
+end a;
