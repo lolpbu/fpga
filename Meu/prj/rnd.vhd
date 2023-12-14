98d48de18153baf5d3f5864d8457c3174ba36406
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-14 19:59:28 -0300

    comentado

--- Meu/prj/rnd.vhd
+++ Meu/prj/rnd.vhd
@@ -1,31 +1,33 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEe;
+use IEEE.std_logic_1164.all;
 
+-- entidade que gera 2 bits pseudo-aleatorios
+-- utilizando um circuito LFSR
 entity rnd is
 port(
-	clk, reset: in std_logic;
-    genned: out  std_logic_vector(1 downto 0)
+	clk, reset: in std_logic; -- clock para a geracao dos bits e reset 
+   genned: out  std_logic_vector(1 downto 0) -- saida para os 2 bits gerados
 );
 end rnd;
 
 architecture gen of rnd is
 
-	signal count_i: std_logic_vector(3 downto 0);
-    signal fb: std_logic;
-
+	signal count_i: std_logic_vector(3 downto 0); -- sinal para saida
+   signal fb: std_logic;                         -- sinal de feedback
+	
 begin
 	fb <= (not count_i(3)) xor count_i(2);
     
-    process(clk, reset) is
-    begin
-    	if reset = '1' then
-        	count_i <= (others=>'0');
-        elsif rising_edge(clk) then
-        	count_i <= count_i(2 downto 0) & fb;
-        end if;
-    end process;
+   process(clk, reset) is
+   begin
+		if reset = '1' then
+			count_i <= (others=>'0');
+		elsif rising_edge(clk) then
+			count_i <= count_i(2 downto 0) & fb;
+		end if;
+	end process;
     
-    genned(1) <= count_i(2);
-    genned(0) <= count_i(1);
+   genned(1) <= count_i(2);
+   genned(0) <= count_i(1);
     
 end gen;
