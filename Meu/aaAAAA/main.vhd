40b11f16313a1c2ef329859905564ccc1c9cb911
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-14 20:09:23 -0300

    com menta

--- Meu/aaAAAA/main.vhd
+++ Meu/aaAAAA/main.vhd
@@ -4,39 +4,22 @@ use ieee.numeric_std.all;
 
 entity cronometro is
 port(
-	clk: in std_logic;
-    enable: in std_logic;
-    min_sec: in std_logic;
-    timer_in: in std_logic_vector(5 downto 0);
-    
-    -- displays
-    min1, min2, sec1, sec2: out std_logic_vector(7 downto 0);
+	clk: in std_logic; -- clock de 50Mhz
+    enable: in std_logic; -- comeca a contagem do tempo
+    min_sec: in std_logic; -- 0: seleciona em segundos, 1: seleciona em minutos
+    timer_in: in std_logic_vector(5 downto 0); -- bits para selecao do tempo
 
     -- lcd
-    LCD_EN: out std_logic;
-    LCD_RW: out std_logic;
-    LCD_RS: out std_logic;
-    LCD_DB: out std_logic_vector(7 downto 0);
+    LCD_EN: out std_logic; -- enable
+    LCD_RW: out std_logic; -- read/write
+    LCD_RS: out std_logic; -- command/data select
+    LCD_DB: out std_logic_vector(7 downto 0); -- data BUS
 
     done: out std_logic_vector(9 downto 0) := "0000000000"
 );
 end cronometro;
 
-architecture bhv of cronometro is
-
-    component display_uni is
-    port(
-        res_in: in std_logic_vector(5 downto 0);
-        disp_out: out std_logic_vector(7 downto 0)
-    );
-    end component;
-
-    component display_dez is
-    port(
-        res_in: in std_logic_vector(5 downto 0);
-        disp_out: out std_logic_vector(7 downto 0)
-    );
-    end component;
+architecture aaa of cronometro is
 
     component LCD is
     port(
@@ -53,38 +36,38 @@ architecture bhv of cronometro is
     );
     end component;
 
-	signal clk_1hz: std_logic;
+	signal clk_1hz: std_logic; -- clock de 1hz
     signal h, m, s: integer range 0 to 63 := 0;
     signal hour, min, sec: std_logic_vector(5 downto 0);
     
 begin
 
-	--1 hz clk def
+	--clock de 1hz
 	process(clk) is
---     	variable cnt: integer := 1;
+     	variable cnt: integer := 1;
     begin
-    	clk_1hz <= clk;
---     	if rising_edge(clk) then
---         	cnt := cnt + 1;
---             if cnt < 50000000 then
---             	clk_1hz <= '0';
---             elsif cnt >= 50000000 and cnt < 100000000 then
---             	clk_1hz <= '1';
---             else
---             	cnt := 1;
---             end if;
---          end if;
+     	if rising_edge(clk) then
+         	cnt := cnt + 1;
+             if cnt < 50000000 then
+             	clk_1hz <= '0';
+             elsif cnt >= 50000000 and cnt < 100000000 then
+             	clk_1hz <= '1';
+             else
+             	cnt := 1;
+             end if;
+          end if;
      end process;
-     
+
+		  
      process(clk_1hz, enable) is
      begin
         
         	if enable = '0' then
-            	if min_sec = '0' then -- sec
+            	if min_sec = '0' then -- segundos
                 	h <= 0;
                     m <= to_integer(unsigned(timer_in)) / 60;
                     s <= to_integer(unsigned(timer_in)) - (m * 60);
-                else -- min
+                else -- minutos
                 	h <= to_integer(unsigned(timer_in)) / 60;
                     m <= to_integer(unsigned(timer_in)) - (h * 60);
                     s <= 0;
@@ -115,11 +98,5 @@ begin
     min  <= std_logic_vector(to_unsigned(m, min'length));
     sec  <= std_logic_vector(to_unsigned(s, sec'length));
 
-    --HDISPDUT: display_uni port map(hour, hour1);
-    M1DISP: display_dez port map(min, min1);
-    M2DISP: display_uni port map(min, min2);
-    S1DISP: display_dez port map(sec, sec1);
-    S2DISP: display_uni port map(sec, sec2);
-
     LCDDUT: LCD port map(clk, hour, min, sec, LCD_EN, LCD_RW, LCD_RS, LCD_DB);
-end bhv;
+end aaa;
