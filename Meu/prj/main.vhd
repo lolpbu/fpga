9a14e361ed6d7c54d5a4ad5b7f2e1cf61edfa1af
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-12-14 19:58:39 -0300

    aaaaaaa

--- Meu/prj/main.vhd
+++ Meu/prj/main.vhd
@@ -1,58 +1,62 @@
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all ;
 
+-- entidade principal
 entity projeto is
 port(
+	-- entrada do clock de 50MHz e o pino de reset
 	clk, reset: in std_logic := '0';
-	a_in, b_in: in std_logic := '0';
-    --sgn: out std_logic_vector(1 downto 0) := "00";
+	
+	-- entradas para os jogadores
+	a_in, b_in: in std_logic := '0'; 
     
-    --pnt_a: out std_logic_vector(3 downto 0) := "0000";
-    --pnt_b: out std_logic_vector(3 downto 0) := "0000";
-    
-	 signs_word: out std_logic_vector(3 downto 0) := "0000";
-	 clk_led: out std_logic := '0';
-	 
-	 pntA_word: out std_logic_vector(3 downto 0);
-	 
-    pnt_a_disp: out std_logic_vector(7 downto 0) := "00000000";
-    pnt_b_disp: out std_logic_vector(7 downto 0) := "00000000"
-    );
+	-- saida contendo os sinais para os pinos GPIO,
+	-- o ultimo bit signals_out(3) contem o sinal correto
+	signals_out: out std_logic_vector(3 downto 0) := "0000";
+	
+	-- saida para os displays de 8 segmentos, demonstrando a pontuacao de cada jogador
+   pntA_disp: out std_logic_vector(7 downto 0) := "00000000";
+   pntB_disp: out std_logic_vector(7 downto 0) := "00000000"
+);
 end projeto;
 
 architecture bhv of projeto is
 	
-   component display is
+	-- componente para os displays
+	component display is
   	port(
     	bin_in: in std_logic_vector(3 downto 0) := "0000";
       disp_out: out std_logic_vector(7 downto 0) := "00000000"
   	);
   	end component;
 	
+	-- componente gerador de 2 bits aleatorios
 	component rnd is
 	port(
 		clk, reset: in std_logic;
 		genned: out  std_logic_vector(1 downto 0)
 	);
 	end component;
-
+ 
+	signal clk_rnd: std_logic := '0';                     -- clock de 1Hz
+   signal sgn_rnd: std_logic_vector(1 downto 0) := "00"; -- 2 bits responsaveis pelos sinais aleatorios, gerado usando o clock de 1Hz
     
-    
-	 signal clk_rnd: std_logic := '0';
-    signal sgn_rnd: std_logic_vector(1 downto 0) := "00";
-    
-    signal pnt_a: std_logic_vector(3 downto 0) := "0000";
-    signal pnt_b: std_logic_vector(3 downto 0) := "0000";
-    
-    signal prv_a: std_logic_vector(3 downto 0) := "0000";
-    signal prv_b: std_logic_vector(3 downto 0) := "0000";
+	signal a_db, b_db: std_logic := '0'; -- debouncer simples para cada entrada de jogador
+	
+	-- pontuacao de cada jogador
+   signal pnt_a: std_logic_vector(3 downto 0) := "0000";
+   signal pnt_b: std_logic_vector(3 downto 0) := "0000";
+	
+	-- pontuacao anterior de cada jogador
+   signal prv_a: std_logic_vector(3 downto 0) := "0000";
+   signal prv_b: std_logic_vector(3 downto 0) := "0000";
 	 
-    signal a_db, b_db: std_logic := '0';
 begin
+	-- ligacao para o componente gerador de 2 bits aleatorio
 	RND_DUT: rnd port map(clk_rnd, reset, sgn_rnd);
 	
+	-- processo de conversao do clock de 50MHz para o clock de 1Hz
 	process(clk) is
 		variable clk_count: integer := 0;
 	begin
@@ -60,54 +64,63 @@ begin
 			clk_count := clk_count + 1;
 			if clk_count < 50000000 then
 				clk_rnd <= '1';
-			elsif clk_count >= 50000000 AND clk_count < 100000000  then
+			elsif clk_count >= 50000000 and clk_count < 100000000  then
 				clk_rnd <= '0';
 			else
 				clk_count := 0;
 			end if;
 		end if;
 	end process;
-			
-	
-
+		
+	-- processo pincipal, responsavel pelo controle e checagem de pontuacao
 	process(clk, reset) is
-    begin
+	begin
 		if reset = '1' then
+			-- reseta as pontuacoes
 			pnt_a <= "0000";
 			pnt_b <= "0000";
 			prv_a <= "0000";
 			prv_b <= "0000";
 		elsif rising_edge(clk) then
+			
+			-- checagem do botao de cada jogador, com um debouncer simples
+			
+			-- jogador 1
 			if a_in = '1' and a_db = '0' then
-				if sgn_rnd = "11" then
+				if sgn_rnd = "11" then 
+					-- sinal   correto, incrementa a pontuacao
 					pnt_a <= std_logic_vector(unsigned(prv_a) + 1);
 					prv_a <= pnt_a;
-				else
+				else 
+					-- sinal incorreto, decrementa a pontuacao
 					if pnt_a /= "0000" then
-					pnt_a <= std_logic_vector(unsigned(prv_a) - 1);
-					prv_a <= pnt_a;
+						pnt_a <= std_logic_vector(unsigned(prv_a) - 1);
+						prv_a <= pnt_a;
 					end if;
 				end if;
-				
 			end if;
 			
+			-- jogador 2
 			if b_in = '1' and b_db = '0' then
 				if sgn_rnd = "11" then
+					-- sinal   correto, incrementa a pontuacao
 					pnt_b <= std_logic_vector(unsigned(prv_b) + 1);
 					prv_b <= pnt_b;
 				else
+					-- sinal incorreto, decrementa a pontuacao
 					if pnt_b /= "0000" then
-					pnt_b <= std_logic_vector(unsigned(prv_b) - 1);
-					prv_b <= pnt_b;
+						pnt_b <= std_logic_vector(unsigned(prv_b) - 1);
+						prv_b <= pnt_b;
 					end if;
 				end if;
-				
 			end if;
 			
 			a_db <= a_in;
 			b_db <= b_in;
 			
-			if pnt_a = "1001" or pnt_b = "1001" then
+			-- caso uma pontuacao ultrapasse o valor maximo,
+			-- as pontuacoes sao resetadas
+			if pnt_a = "1010" or pnt_b = "1010" then
 				pnt_a <= "0000";
 				pnt_b <= "0000";
 				prv_a <= "0000";
@@ -115,30 +128,16 @@ begin
 			end if;
 			
 		end if;
-    end process;
+	end process;
     
-    DISPA_DUT: display port map(pnt_a, pnt_a_disp);
-    DISPB_DUT: display port map(pnt_b, pnt_b_disp);
+	-- conexoes entre as pontuacoes e seus displays 
+   DISPA_DUT: display port map(pnt_a, pntA_disp);
+   DISPB_DUT: display port map(pnt_b, pntB_disp);
 	 
-	 clk_led <= clk_rnd;
-	 pntA_word <= pnt_a;
-	 
-	 with sgn_rnd select
-		signs_word <= "0001" when "00",
-						  "0010" when "01",
-						  "0100" when "10",
-						  "1000" when "11";
---     with pnt_a select
--- 		pnt_a_disp <= "11000000" when "0000", --0
--- 			        "11111001" when "0001", --1
--- 					"10100100" when "0010", --2
--- 					"10110000" when "0011", --3
--- 				    "10011001" when "0100", --4
--- 				    "10010010" when "0101", --5
--- 				    "10000010" when "0110", --6
--- 				    "11111000" when "0111", --7
--- 				    "10000000" when "1000", --8
--- 				    "10010000" when "1001", --9 
--- 				    "00000000" when others;
-    
+	-- definindo os sinais para os pinos GPIO de acordo com os 2 bits aleatorios
+	with sgn_rnd select
+		signals_out <= "0001" when "00",
+						   "0010" when "01",
+						   "0100" when "10",
+						   "1000" when "11";
 end bhv;
